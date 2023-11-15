7907f0988a81b8ab3680d4ae04e15bb779c96492
wilsonandhisgreenbike <quanganhha99@gmail.com>
2023-11-15 15:51:35 +0700

    fix client, masonry, add user avatar skeleton, add unsavepin func

--- swift-share_backend/schemas/save.js
+++ swift-share_backend/schemas/save.js
@@ -3,12 +3,13 @@ export default {
   title: 'Save',
   type: 'document',
   fields: [
+    // thong tin nguoi luu pin
     {
       name: 'postedBy',
       title: 'PostedBy',
       type: 'postedBy',
     },
-    // thong tin nguoi luu pin
+    // thong tin nguoi luu pin (duplicate)
     {
       name: 'userId',
       title: 'UserId',
